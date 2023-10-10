ef9359b72248143c1cdfd2e037db9fd4ea75ebf0
Alastair Poole <netstar@gmail.com>
2023-10-10 13:13:26 +0100

    registrations: end

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,6 +18,7 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+        return $this->redirectToRoute('app_index');
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
